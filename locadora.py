93b0cc7463da2c709778ef4ace0fab59f180c57a
Felipe Vale <85165362+FelipePaivaVale@users.noreply.github.com>
2023-03-08 17:14:32 -0300

    Update locadora.py

--- locadora.py
+++ locadora.py
@@ -1,33 +1,54 @@
+lista_carros = []
+
 class veiculo:
     def __init__(self, marca, modelo, ano):
         self.marca = marca
         self.modelo = modelo
         self.ano = ano
+        self._alugado = False
 
-    def alugar(self, tempo,val_diaria):
-        self.tempo = tempo
-        self.val_diaria = val_diaria
+    def alugar(self, tempo):
+        self.tempo = tempo 
+        self._alugado = True
         
     def devolver(self,dias):
-        atraso = False
+        atrasado = False
 
         if(self.tempo < dias):
             atraso = self.tempo - dias
-            atraso = True
+            atrasado = True
 
-        if(atraso = True):
-            aluguel = self.val_diaria+ ((self.val_diaria/100)*20)*atraso
+        if(atrasado == True):
+            aluguel = self.val_diaria + ((self.val_diaria/100)*20)*atraso
 
         return aluguel
-
+    
+    def alugado(self):
+        if(self._alugado == True):
+            print("o carro esta alugado")
+        else:
+            print("o carro nao esta alugado")
+        
 class carro(veiculo):
     def __init__(self, placa, km, val):
         self.placa = placa
         self.km = km
         self.val = val
-    
 
+    def __str__(self):
+        return print(f'''modelo -> {self.modelo}
+        marca -> {self.marca}
+        ano -> {self.ano}
+        placa -> {self.placa}
+        km rodados -> {self.km}''')
+    
 class cliente(carro):
     def __init__(self, nome, id):
         self.nome = nome
         self.id = id
+
+def cadastrar():
+    marca = str(input("escreva o modelo "))
+    ano = int(input("escreva o ano "))
+    c = carro(marca, ano)
+    lista_carros.append(c)
