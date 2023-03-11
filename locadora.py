8b0c31b0d0de5059ffc8363b8a797f4f5c809ba5
Felipe Vale <85165362+FelipePaivaVale@users.noreply.github.com>
2023-03-11 10:32:45 -0300

    Update locadora.py

--- locadora.py
+++ locadora.py
@@ -2,21 +2,21 @@ lista_carros = []
 
 class veiculo:
     def __init__(self, marca, modelo, ano):
-        self.marca = marca
-        self.modelo = modelo
-        self.ano = ano
+        self._marca = marca
+        self._modelo = modelo
+        self._ano = ano
         self._alugado = False
         
 class carro(veiculo):
     def __init__(self,marca, modelo, ano, placa, km, val):
-        self.placa = placa
-        self.km = km
-        self.val = val
-        super().__init__(marca,modelo, ano)
+        super().__init__(marca, modelo, ano)
+        self._placa = placa
+        self._km = km
+        self._val = val
 
     def alugar(self, tempo):
         self.tempo = tempo 
-        self._alugado = True
+        self.alugado = True
         
     def devolver(self,dias):
         atrasado = False
@@ -26,7 +26,7 @@ class carro(veiculo):
             atrasado = True
 
         if(atrasado == True):
-            aluguel = self.val_diaria + ((self.val_diaria/100)*20)*atraso
+            aluguel = self._val + ((self._val/100)*20)*atraso
 
         return aluguel
     
@@ -37,27 +37,35 @@ class carro(veiculo):
             print("o carro nao esta alugado")
 
     def __str__(self):
-        return f'''modelo -> {self.modelo}
-        marca -> {self.marca}
-        ano -> {self.ano}
-        placa -> {self.placa}
-        km rodados -> {self.km}'''
+        return f"modelo: {self._modelo}\nmarca: {self._marca}\nano: {self._ano}\nplaca: {self._placa}\nquilometros radados: {self._km}\n"
     
 class cliente():
     def __init__(self, nome, id):
         self.nome = nome
         self.id = id
-
+        
 def cadastrar():
-    marca = str(input("escreva o marca "))
-    ano = int(input("escreva o ano "))
-    modelo = str(input("escreva o modelo "))
+    marca = str(input("marca: "))
+    ano = int(input("ano: "))
+    modelo = str(input("modelo: "))
     print("especificações do carro")
-    placa = str(input("qual a placa do carro"))
-    km = int(input("quantos km rodados"))
-    diaria = int(input("qual o valor da diaria"))
-    c = carro(marca, modelo, ano, placa, km, diaria)
-    lista_carros.append(c)
+    placa = str(input("qual a placa do carro: "))
+    km = int(input("quantos km rodados: "))
+    diaria = int(input("qual o valor da diaria: "))
+    lista_carros.append(carro(marca, modelo, ano, placa, km, diaria))
 
-c1 = cadastrar()
-print(c1)
+def cadastro_auto(marca,modelo,ano,placa,km,diaria):
+    lista_carros.append(carro(marca, modelo, ano, placa, km, diaria))
+
+cadastro_auto("fiat","uno",2020,"abc123",0,20)
+cadastro_auto("fiat","riske",2020,"anc343",0,20)
+cadastro_auto("bmw","breske",2020,"akr250",0,20)
+cadastro_auto("bmw","xesque",2020,"kbm345",0,20)
+
+i= 1
+for veiculos in lista_carros:
+    print(i,"=========================")
+    print(veiculos)
+    print('==========================\n')
+    i += 1
+
