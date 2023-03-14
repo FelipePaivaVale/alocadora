cb81713a3cdd82778a92bad3c9f234cae948cf74
Felipe Vale <85165362+FelipePaivaVale@users.noreply.github.com>
2023-03-13 21:40:17 -0300

    Update frontend.py

--- frontend.py
+++ frontend.py
@@ -1,20 +1,4 @@
-# cadastrar, consultar disponibilidade (alugado ou disponivel), listar veiculos,
-# listar veiculos por modelo, listar veiculos por ano
 from locadora import *
-
-
-app.cadastro_auto("fiat", "uno", 2019, "abc123", 0, 30)
-app.cadastro_auto("fiat", "pulse", 2018, "anc343", 0, 30)
-app.cadastro_auto("bmw", "x6", 2010, "akr250", 0, 30)
-app.cadastro_auto("bmw", "x5", 2014, "kbm345", 0, 30)
-app.cadastro_auto("renault", "duster", 2018, "kbm345", 0, 30)
-app.cadastro_auto("renault", "logan", 2011, "kbm77", 0, 30)
-app.cadastro_usuario_auto('pedro')
-app.cadastro_usuario_auto('juse')
-
-def mostrar():
-    for carros in lista_carros:
-        print(carros)
 
 print('Bem-Vindo ao app de carros Lucas Samuel e Felipe\n')
 
@@ -33,7 +17,11 @@ while (True):
     mensagem('8- Cadastrar cliente')
     mensagem('9- Exibir clientes')
     print('-' * 70)
-    operação = int(input('Informe a operação que deseja realizar: '))
+
+    try:
+        operação = int(input('Informe a operação que deseja realizar: '))
+    except:
+        operação = 0
 
     if (operação == 1):
         app.cadastrar()
@@ -52,39 +40,28 @@ while (True):
         input()
     
     elif (operação == 5):
-        print('1-Fiat\n2-Bmw\n3-Renault')
-        escolha = int(input("qual marca deseja procurar? \n"))
+        app.lista_de_marcas()
 
-        if (escolha == 1):
-            marca = 'fiat'
-        elif (escolha == 2):
-            marca = 'bmw'
-        elif (escolha == 3):
-            marca = 'renault'
+        marca = str(input("por qual marca deseja procurar? "))
+
         app.listar_marca(marca)
 
         input()
 
     elif (operação == 6):
-        print('1- Ver modelos da fiat: \n2- Ver modelos da Bmw: \n 3- Ver modelos da renault')
-        escolha = int(input('Deseja ver os modelos de qual marca?'))
+        app.lista_de_modelos()
 
-        if (escolha == 1):
-            print('')
-            modelo = 'fiat'
-        elif (escolha == 2):
-            print('')
-            modelo = 'xesque'
-        elif (escolha == 3):
-            print('')
-            modelo = ''
+        modelo = str(input("por qual modelo deseja procurar? "))
             
         app.listar_modelo(modelo)
+
         input()
 
     elif(operação == 7):
         ano = int(input("de qual ano deseja ver os carros"))
+
         app.listar_ano(ano)
+
         input()
 
     elif(operação == 8):
@@ -97,6 +74,3 @@ while (True):
 
     else:
         print('Operação invalida')
-
-# qual a marca
-# qual modelod a marca
